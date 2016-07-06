33445a721cac32d8cc8d79f540fedb6ef4308a25
MindfulBell <tim.bell02@gmail.com>
2016-07-06 16:18:49 -0700

    es6 nonsense

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var PORT = process.env.PORT || 8080;
-var favicon = require('serve-favicon');
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const favicon = require('serve-favicon');
 
-var app = express();
+let app = express();
 app.use(favicon(__dirname + '/public/favicon.ico'))
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
 	res.send('Todo API Root');
 })
 
-app.listen(PORT, function(req, res){
-	console.log('Listening on PORT ' + PORT)
+app.listen(PORT, (req, res)=>{
+	console.log(`Listening on PORT ${PORT}`)
 })
