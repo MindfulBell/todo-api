b662f21dd1be16f233bc4b979d244ead53ac6331
MindfulBell <tim.bell02@gmail.com>
2016-07-09 16:28:17 -0700

    wired up get todos route with search/filter

--- server.js
+++ server.js
@@ -19,26 +19,21 @@ app.get('/', (req, res) => {
 // GET /todos?completed=true&q=ass
 
 app.get('/todos', (req, res) => {
-	let queryParams = req.query;
-	var filteredTodos = todos;
+	let query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty("completed") && queryParams.completed === "true") {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		})
-	} else if (queryParams.hasOwnProperty("completed") && queryParams.completed === "false") {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		})
+	if (query.hasOwnProperty("completed")){
+		where.completed = query.completed === "true" ? true : false;
+	}
+	if (query.hasOwnProperty("q") && query.q.trim().length > 0){
+		where.description = { $like: `%${query.q.trim()}%` }
 	}
 
-	if (queryParams.hasOwnProperty("q") && queryParams.q.trim().length > 0) {
-		filteredTodos = _.filter(filteredTodos, (todo) => {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1
-		})
-	}
-
-	res.json(filteredTodos);
+	db.todo.findAll({where}).then((todos)=>{
+			res.json(todos)		
+	}).catch((e)=>{
+		res.status(500).send();
+	})
 })
 
 //GET todos/:id
